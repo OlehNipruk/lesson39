8ec67305606401adee09742d699d284ad7d56d0f
Oleh Nipruk <nipruk@ukr.net>
2023-05-15 14:00:40 +0300

    lesson 39 correction

--- src/main/java/com/ua/robot/lesson39/rest/StudentController.java
+++ src/main/java/com/ua/robot/lesson39/rest/StudentController.java
@@ -2,19 +2,16 @@ package com.ua.robot.lesson39.rest;
 
 import com.ua.robot.lesson39.domain.Student;
 import com.ua.robot.lesson39.service.StudentService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @RestController
 public class StudentController {
     private final StudentService studentService;
-
-    public StudentController(StudentService studentService) {
-        this.studentService = studentService;
-    }
-
     @GetMapping("/students")
     public List<Student> getStudents() {
         return studentService.getStudents();
